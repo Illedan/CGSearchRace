16ba6ace10c2354f869fb54c8dc44d2a457d1f79
Erik Kvanli <0erikkvanli0@gmail.com>
2020-05-21 21:19:16 +0200

    Less and better validators

--- SearchRace/src/test/java/SkeletonMain.java
+++ SearchRace/src/test/java/SkeletonMain.java
@@ -13,7 +13,7 @@ public class SkeletonMain {
          gameRunner.setAgent(Agent1.class);
 
         // Sets a test case
-         gameRunner.setTestCase("test1.json");
+         gameRunner.setTestCase("test7.json");
 
         // Another way to add a player
         // gameRunner.addAgent("python3 /home/user/player.py");
